384c92cdfa4f32e4efb03b3f88e5662e9256ae82
arymuhammad <ademuhammad12@gmail.com>
2023-04-11 15:42:43 +0700

    update login view

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -11,72 +11,97 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('LOGIN'),
-          centerTitle: true,
-        ),
+        // appBar: AppBar(
+        //   title: const Text('LOGIN'),
+        //   centerTitle: true,
+        // ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: ListView(
-            children: [
-              const SizedBox(height: 35),
-              Center(
-                child: ClipOval(
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(color: Colors.grey[300]),
-                    child: Image.asset(
-                      "assets/image/selfie.png",
-                      fit: BoxFit.contain,
+      padding: const EdgeInsets.symmetric(vertical: 35),
+      child: ListView(
+        padding: MediaQuery.of(context).size.width > 1100
+            ? const EdgeInsets.symmetric(horizontal: 500)
+            : const EdgeInsets.symmetric(horizontal: 50),
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.white),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 35),
+                  Center(
+                    child: ClipOval(
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(color: Colors.grey[300]),
+                        child: Image.asset(
+                          "assets/image/selfie.png",
+                          fit: BoxFit.contain,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  const Center(
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(fontSize: 22),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(
+                    controller: controller.username,
+                    decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Username',
+                        prefixIcon: Icon(TernavIcons.light.profile)),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Obx(
+                    () => TextField(
+                      controller: controller.password,
+                      obscureText: controller.isPassHide.value,
+                      decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          labelText: 'Password',
+                          prefixIcon: Icon(TernavIcons.bold.lock),
+                          suffixIcon: InkWell(
+                              onTap: () {
+                                controller.isPassHide.value =
+                                    !controller.isPassHide.value;
+                              },
+                              child: Icon(controller.isPassHide.value
+                                  ? Icons.visibility
+                                  : Icons.visibility_off))),
+                      onSubmitted: (v) => controller.login(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: Size(Get.mediaQuery.size.width, 50)),
+                      onPressed: () => controller.login(),
+                      child: const Text('LOGIN')),
+                  const SizedBox(height: 20),
+                  TextButton(
+                      onPressed: () {
+                        Get.to(() => VerifikasiUpdatePassword(),
+                            transition: Transition.cupertino);
+                      },
+                      child: const Text('Lupas Password?')),
+                ],
               ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: controller.username,
-                decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'Username',
-                    prefixIcon: Icon(TernavIcons.light.profile)),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Obx(()=> TextField(
-                  controller: controller.password,
-                  obscureText: controller.isPassHide.value,
-                  decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      labelText: 'Password',
-                      prefixIcon: Icon(TernavIcons.bold.lock),
-                      suffixIcon: InkWell(
-                          onTap: () {
-                            controller.isPassHide.value =
-                                !controller.isPassHide.value;
-                          },
-                          child: Icon(controller.isPassHide.value?Icons.visibility:Icons.visibility_off))),
-                  onSubmitted: (v) => controller.login(),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: Size(Get.mediaQuery.size.width, 50)),
-                  onPressed: () => controller.login(),
-                  child: const Text('LOGIN')),
-              const SizedBox(height: 20),
-              TextButton(
-                  onPressed: () {
-                    Get.to(() => VerifikasiUpdatePassword(),
-                        transition: Transition.cupertino);
-                  },
-                  child: const Text('Lupas Password?')),
-            ],
-          ),
-        ));
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
